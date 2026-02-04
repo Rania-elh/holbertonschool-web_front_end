317c158cba8b7a230565a9e4fd1e20297b4a1e97
Rania-elh <rania1elhefnawy@gmail.com>
2026-02-04 15:27:24 +0100

    projet

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,7 @@
+function welcome (firstName, lastName) {
+	let fullName = fistName + ' ' + lastName;
+	function displayFullName () {
+		alert('welcome ' + fullName + '!');
+	}
+	displayFullName();
+}
